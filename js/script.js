e980104062009f566a9ea58152f8d1f5aff2f82f
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-22 19:47:21 +0100

    got template working with response

--- js/script.js
+++ js/script.js
@@ -23,14 +23,30 @@ function searchCountries() {
 function showCountriesList(resp) {
     var response = resp[0];
 
-    console.log(response);
-    console.log(response.capital + " " + response.languages[0].iso639_1);
     countriesList.innerHTML = '';
     resp.forEach(function(item){
+        var check = loadAll(response);
         var liEl = document.createElement('li');
-        liEl.innerText = item.name;
+        liEl.innerHTML = item.check;
         countriesList.appendChild(liEl);
+        
+        console.log(check);
     });
+   
 }
 
 
+function loadAll (response) {
+    var template = document.getElementById('template').innerHTML;
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, {image: response.flag,
+        name: response.name,
+        capital: response.capital,
+        region: response.region,
+        population: response.population,
+        currencies: response.currencies[0].name,
+        languages: response.languages[0].name + " - " + response.languages[0].nativeName,
+        zone: response.timezones[0]
+    });
+    return rendered;
+}
